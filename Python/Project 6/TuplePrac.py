cfd8af468fcea1903b00da5131a0502ed67e980e
SC Rafi <117970297+MSCRAFI@users.noreply.github.com>
2022-12-28 21:40:19 +0600

    Add files via upload

---
+++ Python/Project 6/TuplePrac.py
@@ -0,0 +1,10 @@
+tup=(43,3,534,32,645,45,3,5,45,3,534,3)
+tup2=tup[1:3]
+print(tup2)
+CountTup=tup.count(3)
+print(CountTup)
+TupLen=len(tup)
+if 645 in tup:
+ print("645 is present in this Tuple.")
+else:
+ print("645 is not present in this Tuple.")
