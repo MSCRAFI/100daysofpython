1be312e75ade1fc6e759c7be136c2c35b7a7c55e
SC Rafi <117970297+MSCRAFI@users.noreply.github.com>
2023-01-18 15:59:15 +0600

    Delete Project 14 directory

--- Project 14/listdir.py
+++
@@ -1,6 +0,0 @@
-import os
-
-folders = os.listdir("New Files")
-
-for folder in folders:
-    print(folder)
--- Project 14/rename.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if not os.path.exists("New Files"):
-    os.mkdir("New Files")
-for i in range(0, 100):
-    os.rename(f"New Files/Day {i+1}, New Files/Project {i+1}")
